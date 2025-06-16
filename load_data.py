f760d4bf65fbfa7c0f4066b88f39c1d1def72c93
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-06-16 15:28:03 -0500

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,10 +1,6 @@
 import os
 
 def get_html_file_map(folder="lda_results"):
-    """Returns dict: {display_name: filename}"""
     html_files = [f for f in os.listdir(folder) if f.endswith(".html")]
-    display_names = [
-        f.replace("lda_visualization_", "").replace(".html", "")
-        for f in html_files
-    ]
+    display_names = [f.replace("lda_visualization_", "").replace(".html", "") for f in html_files]
     return dict(zip(display_names, html_files))
